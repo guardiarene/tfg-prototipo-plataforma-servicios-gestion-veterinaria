e8e05683e033afdc9747717bb5cb6b8f8e4d3560
René Guardia <reneguardia@outlook.com>
2025-06-20 19:00:03 -0300

    refactor: translated to english

--- src/main/java/tfg/prototipo/repositorio/VacunaRepository.java
+++
@@ -1,9 +0,0 @@
-package tfg.prototipo.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tfg.prototipo.modelo.Vacuna;
-
-@Repository
-public interface VacunaRepository extends JpaRepository<Vacuna, Long> {
-}
