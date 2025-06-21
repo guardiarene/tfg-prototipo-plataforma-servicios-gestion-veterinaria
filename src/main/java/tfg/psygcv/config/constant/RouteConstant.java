ea4fb838f482fed587b5d51f0e15a40ead2108d4
René Guardia <reneguardia@outlook.com>
2025-06-21 16:43:51 -0300

    refactor(controller): added RouteConstant

---
+++ src/main/java/tfg/psygcv/config/constant/RouteConstant.java
@@ -0,0 +1,41 @@
+package tfg.psygcv.config.constant;
+
+public final class RouteConstant {
+
+    public static final String REDIRECT_LOGIN = "redirect:/users/login";
+
+    public static final String REDIRECT_LOGIN_LOGOUT = "redirect:/users/login?logout";
+
+    public static final String REDIRECT_ADMIN_DASHBOARD = "redirect:/admin/dashboard";
+
+    public static final String REDIRECT_CLIENT_DASHBOARD = "redirect:/client/dashboard";
+
+    public static final String REDIRECT_VETERINARIAN_DASHBOARD = "redirect:/veterinarian/dashboard";
+
+    public static final String REDIRECT_RECEPTIONIST_DASHBOARD = "redirect:/receptionist/dashboard";
+
+    public static final String REDIRECT_MY_PROFILE = "redirect:/profile";
+
+    public static final String REDIRECT_MY_PETS = "redirect:/pets";
+
+    public static final String REDIRECT_MY_APPOINTMENTS = "redirect:/appointments";
+
+    public static final String REDIRECT_MY_SERVICES = "redirect:/medical-services";
+
+    public static final String REDIRECT_MY_CLINIC = "redirect:/veterinarian/clinic";
+
+    public static final String VIEW_USER_REGISTER = "users/register";
+
+    public static final String VIEW_USER_LOGIN = "users/login";
+
+    public static final String VIEW_ADMIN_NEW_USER = "admin/new_user";
+
+    public static final String VIEW_ADMIN_EDIT_USER = "admin/edit_user";
+
+    public static final String VIEW_ADMIN_DASHBOARD = "admin/dashboard";
+
+    private RouteConstant() {
+
+    }
+
+}
