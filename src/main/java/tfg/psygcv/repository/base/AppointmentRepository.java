2c19297280db614257f435a9b73a5dcb5be71c29
René Guardia <reneguardia@outlook.com>
2026-03-29 12:59:42 -0300

    fix(imports): update package structure from 'model' to 'entity' in Appointment repositories for consistency

--- src/main/java/tfg/psygcv/repository/base/AppointmentRepository.java
+++ src/main/java/tfg/psygcv/repository/base/AppointmentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import tfg.psygcv.model.appointment.Appointment;
+import tfg.psygcv.entity.appointment.Appointment;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
--- src/main/java/tfg/psygcv/repository/query/AppointmentQueryRepository.java
+++ src/main/java/tfg/psygcv/repository/query/AppointmentQueryRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import tfg.psygcv.model.appointment.Appointment;
-import tfg.psygcv.model.clinic.VeterinaryClinic;
-import tfg.psygcv.model.pet.Pet;
-import tfg.psygcv.model.user.User;
+import tfg.psygcv.entity.appointment.Appointment;
+import tfg.psygcv.entity.clinic.VeterinaryClinic;
+import tfg.psygcv.entity.pet.Pet;
+import tfg.psygcv.entity.user.User;
 
 @Repository
 public interface AppointmentQueryRepository extends JpaRepository<Appointment, Long> {
